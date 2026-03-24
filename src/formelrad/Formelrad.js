a662eb5d9439ce2b0e695d592264327c1f487b03
Timure228 <arductimur@gmail.com>
2026-03-24 21:01:25 +0100

    Power is also required

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -1,13 +1,15 @@
 import {useState} from "react";
+import '../css/mvp.css';
 import formelrad from "../image/formelradelektronik.gif";
+import InputField from "../formular/InputField";
 
 export default function Formelrad() {
     const [values, setValues] = useState({
         u: 10,
         i: 2,
-        r: ""
+        r: "",
+        p: ""
     })
-
     return (
         <>
             <section>
@@ -16,15 +18,10 @@ export default function Formelrad() {
                     <img src={formelrad} width="200" alt="Formelrad"/>
                 </header>
                 <form>
-                    <div>
-                        <label>Spannung</label><input></input>
-                    </div>
-                    <div>
-                        <label>Strom</label><input></input>
-                    </div>
-                    <div>
-                        <label>Widerstand</label><input></input>
-                    </div>
+                    <InputField color={"black"} value={values.u} label="Spannung" handleChange={e => {setValues(values => ({...values, u: e.target.value}))}} />
+                    <InputField color={"black"} value={values.i} label="Stromstärke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
+                    <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
+                    <InputField color={"black"} value={values.p} label="Leistung" handleChange={e => {setValues(values => ({...values, p: e.target.value}))}} />
                     <button type="submit">Calculate</button>
                 </form>
             </section>
